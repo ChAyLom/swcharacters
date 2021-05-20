31607e4e98cf1af525c8206ccd73460e0eaf106d
ChAyLom <ilya_chavlytko@mail.ru>
2021-05-20 21:23:45 +0700

    Finalize intl inject

--- src/Components/PeopleCard/PeopleCard.tsx
+++ src/Components/PeopleCard/PeopleCard.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { People } from '../../entities';
 import UnknownCheck from '../UnknownCheck';
 import styles from './PeopleCard.scss';
@@ -14,17 +15,27 @@ const PeopleCard: FC<Props> = ({
     <div className={styles.card}>
       <div className={styles.name}>{people.name}</div>
       <div className={styles.info}>
-        <div>Birth year - {people.birth_year}</div>
-        <div>Gender - {people.gender}</div>
-        <div>Height - <UnknownCheck value={people.height}>
-          {v => `${v} cm`}
-        </UnknownCheck></div>
-        <div>Mass - <UnknownCheck value={people.mass}>
-          {v => `${v} kg`}
-        </UnknownCheck></div>
-        <div>Skin color - {people.skin_color}</div>
-        <div>Eye color - {people.eye_color}</div>
-        <div>Hair color - {people.hair_color}</div>
+        <div><FormattedMessage id="birthYear"/> - {people.birth_year}</div>
+        <div><FormattedMessage id="gender"/> - {people.gender}</div>
+        <div>
+          <FormattedMessage id="height"/> - <UnknownCheck value={people.height}>
+            {value => <FormattedMessage
+              id="distanceCm"
+              values={{ value }}
+            />}
+          </UnknownCheck>
+        </div>
+        <div>
+          <FormattedMessage id="mass"/> - <UnknownCheck value={people.mass}>
+            {value => <FormattedMessage
+              id="massKg"
+              values={{ value }}
+            />}
+          </UnknownCheck>
+        </div>
+        <div><FormattedMessage id="skinColor"/> - {people.skin_color}</div>
+        <div><FormattedMessage id="eyeColor"/> - {people.eye_color}</div>
+        <div><FormattedMessage id="hairColor"/> - {people.hair_color}</div>
       </div>
     </div>
   </div>;
